531c4a47ae0f9f4ae5d2fc4b9edade6c5566836e
JKOC07 <johnkristoffer.camacho@sjsu.edu>
2026-04-23 13:39:19 -0700

    removing

--- server/dist/match/util.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const clamp: (n: number, min: number, max: number) => number;
-export declare const clamp01: (n: number) => number;
-export declare function roundTo(n: number, decimals: number): number;
-//# sourceMappingURL=util.d.ts.map
--- server/dist/protocol/wire.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ServerMessage } from "./schema.js";
-export declare function encodeServerMessage(msg: ServerMessage): string;
-export declare function safeJsonParse(raw: string): unknown;
-//# sourceMappingURL=wire.d.ts.map
