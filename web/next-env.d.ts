56b21fcd2feebaee2feda40a42200558b76eb1ad
JKOC07 <johnkristoffer.camacho@sjsu.edu>
2026-05-05 13:39:41 -0700

    Assets

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import ".//var/folders/dc/qty49k9s42g35fsrd1pbgsq00000gn/T/pleasanton-next-dev-744cefe1b9d5/dev/types/routes.d.ts";
+import "./.next/dev/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
